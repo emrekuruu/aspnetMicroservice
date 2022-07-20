b67cbb2d04a112363caf324de258e9db16d0c4d7
Emre Kuru <emre.kuru@ozu.edu.tr>
2022-07-20 11:28:07 +0300

    Basket API

---
+++ src/Services/Basket/Basket.API/Controllers/BasketController.cs
@@ -0,0 +1,49 @@
+﻿using System.Net;
+using Basket.API.Entities;
+using Basket.API.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Basket.API.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class BasketController : ControllerBase
+    {
+
+        private readonly IBasketRepository _basketRepository;
+
+        public BasketController(IBasketRepository basketRepository)
+        {
+            _basketRepository = basketRepository ?? throw new ArgumentNullException(nameof(basketRepository));
+        }
+
+        [HttpGet("{userName}", Name = "GetBasket")]
+        [ProducesResponseType(typeof(ShoppingCart), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<ShoppingCart>> GetBasket(string userName)
+        {
+            var basket = await _basketRepository.GetBasket(userName);
+            if (basket == null) basket = new ShoppingCart(userName);
+            return Ok(basket);
+        }
+
+        [HttpPost]
+        [ProducesResponseType(typeof(ShoppingCart),(int)HttpStatusCode.OK)]
+        public async Task<ActionResult<ShoppingCart>> UpdateBasket([FromBody] ShoppingCart basket)
+        {
+            return Ok(await _basketRepository.UpdateBasket(basket));
+        }
+
+
+        [HttpDelete( "{user}", Name = "DeleteBasket")]
+        [ProducesResponseType(typeof(void),(int)HttpStatusCode.OK)]
+        public async Task<IActionResult> DeleteBasket(string user)
+        {
+            await _basketRepository.DeleteBasket(user);
+            return Ok();    
+        }
+
+
+    }
+
+}
---
+++ src/Services/Basket/Basket.API/Repositories/IBasketRepository.cs
@@ -0,0 +1,12 @@
+﻿using Basket.API.Entities;
+
+namespace Basket.API.Repositories
+{
+    public interface IBasketRepository
+    {
+        Task<ShoppingCart> GetBasket(string user);
+        Task<ShoppingCart> UpdateBasket(ShoppingCart cart);
+
+        Task DeleteBasket(string user);
+    }
+}
